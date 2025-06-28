b24f85aa15fbca9fea6095e9c59a2b0aa78a7542
Papycyclopede <skillhive.formation@gmail.com>
2025-06-28 19:03:36 +0200

    Updated _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, Settings, User } from 'lucide-react-native';
+import { Chrome as Home, Settings, User } from 'lucide-react-native';
 
 export default function TabLayout() {
   return (
